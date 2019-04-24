4d420faa5535e725a717aa3b44396980501d919c
Graham Claffey <littleendianroot@gmail.com>
2019-04-24 20:25:42 +0100

    Add files via upload

--- DigitalSystems4ED5502/STM32_Projects/1Blinky/Objects/main.d
+++ DigitalSystems4ED5502/STM32_Projects/1Blinky/Objects/main.d
@@ -1,11 +1,9 @@
-./objects/main.o: main.c \
-  C:/Keil_v5/ARM/PACK/Keil/STM32L4xx_DFP/2.2.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include\stm32L476xx.h \
-  C:/Keil_v5/ARM/PACK/ARM/CMSIS/5.5.1/CMSIS/Core/Include\core_cm4.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:/Keil_v5/ARM/PACK/ARM/CMSIS/5.5.1/CMSIS/Core/Include/cmsis_version.h \
-  C:/Keil_v5/ARM/PACK/ARM/CMSIS/5.5.1/CMSIS/Core/Include/cmsis_compiler.h \
-  C:/Keil_v5/ARM/PACK/ARM/CMSIS/5.5.1/CMSIS/Core/Include/cmsis_armclang.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  C:/Keil_v5/ARM/PACK/ARM/CMSIS/5.5.1/CMSIS/Core/Include/mpu_armv7.h \
-  C:/Keil_v5/ARM/PACK/Keil/STM32L4xx_DFP/2.2.0/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+.\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32L4xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32L476xx.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32L4xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
--- DigitalSystems4ED5502/STM32_Projects/1Blinky/main.c
+++ DigitalSystems4ED5502/STM32_Projects/1Blinky/main.c
@@ -6,18 +6,19 @@ int main()
 {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 	
-	GPIOA->MODER &= GPIO_MODER_MODE5;
+	GPIOA->MODER &= ~GPIO_MODER_MODE5;
 	GPIOA->MODER |= GPIO_MODER_MODE5_0;
 	
 	for(;;)
 	{
-		GPIOA->IDR |= GPIO_BSRR_BS5;
+		GPIOA->BSRR |= GPIO_BSRR_BS5;
 		delayMs(500);
-		GPIOA->IDR &= ~GPIO_BSRR_BR5;
+		GPIOA->BSRR |= GPIO_BSRR_BR5;
 		delayMs(500);
 	}
 	
 	return 0;
+
 }
 
 void delayMs(int n)
@@ -25,6 +26,6 @@ void delayMs(int n)
 	int i;
 	for(;n > 0; n--)
 	{
-		for(i = 0; i < 80000; i++);
+		for(i = 0; i < 1000; i++);
 	}
 }
